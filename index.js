c70d916a13e39905ddf05b87e3bfa64608dcce6b
nahashon <nashonmwendwa0@gmail.com>
2024-07-01 10:39:16 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {
+    name: "Max",
+    streetAddress: "5 Main Street",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Alternate using ES6 Spread operators:
+    // return { ...employee, ...{ [key]: value } }
+    return Object.assign({}, employee, { [key]: value });
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    // Alternate using ES6 Spread operator
+  }  
